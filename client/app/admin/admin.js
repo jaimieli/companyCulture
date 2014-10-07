333c69aa686c98a499a419e1d87aac1ecfd9d68c
Jaimie Li <jaimieli@gmail.com>
2014-10-07 09:47:43 -0400

    first commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('companyCultureApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
