18d9ce6fbbbee0e964eacdef477d7b3cd88defa6
Summer McDonald <summermichiko@gmail.com>
2014-10-08 12:33:52 -0400

    generating members and answers for match q

--- client/app/app.js
+++ client/app/app.js
@@ -6,7 +6,8 @@ angular.module('companyCultureApp', [
   'ngSanitize',
   'btford.socket-io',
   'ui.router',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'ngDragDrop'
 ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
     $urlRouterProvider
--- client/app/game/game.controller.js
+++ client/app/game/game.controller.js
@@ -2,9 +2,16 @@
 
 angular.module('companyCultureApp')
   .controller('GameCtrl', function ($scope, $http) {
-
-
-     $http.get('/api/questions').success(function(questionsArr) {
-         $scope.questionsArr = questionsArr;
+     $http.get('/api/questions').success(function(questionsArray) {
+         $scope.questionsArray = questionsArray;
      });
+     $http.get('/api/groups').success(function(groupData) {
+         $scope.groupData = groupData;
+         // console.log("Group Data:", groupData);
+         // console.log("QuestionsArr 0: ", groupData[groupData.length-1].questionsArr[0]);
+         // console.log("QuestionsArr: ", questionsArr);
+     });
+     $scope.random = function() {
+      return 0.5 - Math.random();
+     }
   });
