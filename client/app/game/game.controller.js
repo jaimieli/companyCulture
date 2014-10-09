55c46a2ac82d72edadcb0932e7ebcd28f5bf6bec
Summer McDonald <summermichiko@gmail.com>
2014-10-09 16:49:57 -0400

    drag drop working without random func

--- client/app/game/game.controller.js
+++ client/app/game/game.controller.js
@@ -11,17 +11,82 @@ angular.module('companyCultureApp')
               var obj = {user: groupData[groupData.length-1].questionsArr[groupData[groupData.length-1].questionsArr.length-1].answersArr[i].user};
               $scope.users.push(obj);
               $scope.blanks.push({answer: groupData[groupData.length-1].questionsArr[groupData[groupData.length-1].questionsArr.length-1].answersArr[i].answer});
+              $scope.bottomArr.push({answer: groupData[groupData.length-1].questionsArr[groupData[groupData.length-1].questionsArr.length-1].answersArr[i].answer});
+              // $scope.blanks[i].user = (groupData[groupData.length-1].questionsArr[groupData[groupData.length-1].questionsArr.length-1].answersArr[i].user);
           };
-            console.log("users:", $scope.users);
-            console.log("answers:", $scope.answers);
        });
-     $scope.random = function() {
-      return 0.5 - Math.random();
-     };
 
      $scope.users = [];
      $scope.blanks = [];
      $scope.answers = [];
+     $scope.grabbed = "";
+     $scope.dropped = "";
+     $scope.bottomArr = [];
+
+     $scope.grabbedItem = function(event, ui, grabbedItem) {
+      $scope.grabbed = grabbedItem;
+     };
+
+     $scope.droppedItem = function(event, ui, droppedItem, index){
+        $scope.dropped = $scope.grabbed;
+        $scope.checkDiff();
+        $scope.checkAnswer();
+     }
+
+     $scope.clearItem = function(event, ui, clearedItem, index) {
+      delete $scope.bottomArr[index].user;
+     };
+
+     $scope.checkDiff = function() {
+        for (var i = 0; i < $scope.blanks.length; i++){
+          if($scope.blanks[i].user){
+            $scope.bottomArr[i].user = $scope.blanks[i].user;
+          }else{
+            delete $scope.bottomArr[i].user;
+          };
+        }
+     };
+
+
+     $scope.checkAnswer = function(){
+     // $scope.shortGroupData = $scope.groupData[$scope.groupData.length-1].questionsArr[$scope.groupData[$scope.groupData.length-1].questionsArr.length-1];
+
+      $scope.right = [];
+
+      for(var x = 0; x < $scope.bottomArr.length; x++) {
+        if($scope.bottomArr[x].user === $scope.groupData[$scope.groupData.length-1].questionsArr[$scope.groupData[$scope.groupData.length-1].questionsArr.length-1].answersArr[x].user) {
+          console.log("correct");
+          $scope.right.push("success");
+        }else{
+           $scope.right.push("danger");
+        }
+
+      }
+
+
+     }
+
+     // make a function that takes individual answer as the argument
+     // compare it with the actual answer, return true or false
+     // based on what it returns, chage the class
 
   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
