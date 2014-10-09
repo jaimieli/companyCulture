af439d2e0a5671a4a9386d95c222740aa4e16164
Summer McDonald <summermichiko@gmail.com>
2014-10-09 19:14:27 -0400

    before modal added

--- client/app/game/game.controller.js
+++ client/app/game/game.controller.js
@@ -6,21 +6,22 @@ angular.module('companyCultureApp')
     // TIMER
     $scope.timerSeconds = 0;
     $scope.$on('timer-tick', function(event, value) {
-      $scope.timerSeconds = 60 - (Math.floor(value.millis / 1000)) % 60;
+      $scope.timerSeconds = 90 - (Math.floor(value.millis / 1000)) % 90;
     });
     $scope.interval = 0;
     $interval(function() {
       $scope.interval = ($scope.interval + 1) % 100;
     }, 1000);
 
-     $http.get('/api/questions').success(function(questionsArray) {
-         $scope.questionsArray = questionsArray;
-     });
+    // REQUESTS FOR DATA FROM SEED
+    $http.get('/api/questions').success(function(questionsArray) {
+      $scope.questionsArray = questionsArray;
+    });
 
-     var shuffle = function(o) {
-            for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-            return o;
-          };
+    var shuffle = function(o) {
+      for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+      return o;
+    };
 
      $http.get('/api/groups').success(function(groupData) {
          $scope.groupData = groupData;
@@ -48,7 +49,7 @@ angular.module('companyCultureApp')
       $scope.dropped = $scope.grabbed;
       $scope.checkDiff();
       $scope.checkAnswer();
-     }
+     };
 
      $scope.clearItem = function(event, ui, clearedItem, index) {
       delete $scope.bottomArr[index].user;
@@ -73,7 +74,17 @@ angular.module('companyCultureApp')
            $scope.right.push("danger");
         }
       }
-     }
+      var correctCounter = 0;
+      for(var x = 0; x < $scope.bottomArr.length; x++) {
+        if($scope.bottomArr[x].user === $scope.groupData[$scope.groupData.length-1].questionsArr[$scope.groupData[$scope.groupData.length-1].questionsArr.length-1].answersArr[x].user) {
+          correctCounter++;
+        }
+      }
+      if(correctCounter == $scope.bottomArr.length){
+        console.log("got it all");
+        //modal pop up with elapsed time and buttons to go to leader boards
+      };
+     };
 
      $scope.reset = function() {
       for(var t = 0; t < $scope.bottomArr.length; t++) {
@@ -82,11 +93,8 @@ angular.module('companyCultureApp')
          $scope.users[t].user = $scope.groupData[$scope.groupData.length-1].questionsArr[$scope.groupData[$scope.groupData.length-1].questionsArr.length-1].answersArr[t].user;
       }
       $scope.users = shuffle($scope.users);
-     };
-
-
-
-  });
+  };
+});
 
 
 
