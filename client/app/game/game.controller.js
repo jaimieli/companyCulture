21ed26f35b4674ecb30f9305c21e903ae62f793b
Summer McDonald <summermichiko@gmail.com>
2014-10-09 10:58:09 -0400

    drag drop with answers working

--- client/app/game/game.controller.js
+++ client/app/game/game.controller.js
@@ -8,18 +8,19 @@ angular.module('companyCultureApp')
      $http.get('/api/groups').success(function(groupData) {
          $scope.groupData = groupData;
          for(var i = 0; i < groupData[groupData.length-1].questionsArr[groupData[groupData.length-1].questionsArr.length-1].answersArr.length; i++){
-           var obj = {user: groupData[groupData.length-1].questionsArr[groupData[groupData.length-1].questionsArr.length-1].answersArr[i].user};
+              var obj = {user: groupData[groupData.length-1].questionsArr[groupData[groupData.length-1].questionsArr.length-1].answersArr[i].user};
               $scope.users.push(obj);
-              $scope.answers.push({});
+              $scope.blanks.push({answer: groupData[groupData.length-1].questionsArr[groupData[groupData.length-1].questionsArr.length-1].answersArr[i].answer});
           };
-            console.log($scope.users);
-            console.log($scope.answers);
+            console.log("users:", $scope.users);
+            console.log("answers:", $scope.answers);
        });
      $scope.random = function() {
       return 0.5 - Math.random();
      };
 
      $scope.users = [];
+     $scope.blanks = [];
      $scope.answers = [];
 
   });
