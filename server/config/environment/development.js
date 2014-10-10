31de86a7eebbb5a0acf9e53554a0df1cebcd0d9e
Jaimie Li <jaimieli@gmail.com>
2014-10-10 16:32:49 -0400

    changed to not use seed data functions

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -7,6 +7,5 @@ module.exports = {
   mongo: {
     uri: 'mongodb://localhost/companyculture-dev'
   },
-
   seedDB: false
 };
--- server/config/seed.js
+++ server/config/seed.js
@@ -12,73 +12,73 @@ var Question = require('../api/question/question.model');
 var Game = require('../api/game/game.model');
 
 
-Thing.find({}).remove(function() {
-  Thing.create({
-    name : 'Development Tools',
-    info : 'Integration with popular tools such as Bower, Grunt, Karma, Mocha, JSHint, Node Inspector, Livereload, Protractor, Jade, Stylus, Sass, CoffeeScript, and Less.'
-  }, {
-    name : 'Server and Client integration',
-    info : 'Built with a powerful and fun stack: MongoDB, Express, AngularJS, and Node.'
-  }, {
-    name : 'Smart Build System',
-    info : 'Build system ignores `spec` files, allowing you to keep tests alongside code. Automatic injection of scripts and styles into your index.html'
-  },  {
-    name : 'Modular Structure',
-    info : 'Best practice client and server structures allow for more code reusability and maximum scalability'
-  },  {
-    name : 'Optimized Build',
-    info : 'Build process packs up your templates as a single JavaScript payload, minifies your scripts/css/images, and rewrites asset names for caching.'
-  },{
-    name : 'Deployment Ready',
-    info : 'Easily deploy your app to Heroku or Openshift with the heroku and openshift subgenerators'
-  });
-});
+// Thing.find({}).remove(function() {
+//   Thing.create({
+//     name : 'Development Tools',
+//     info : 'Integration with popular tools such as Bower, Grunt, Karma, Mocha, JSHint, Node Inspector, Livereload, Protractor, Jade, Stylus, Sass, CoffeeScript, and Less.'
+//   }, {
+//     name : 'Server and Client integration',
+//     info : 'Built with a powerful and fun stack: MongoDB, Express, AngularJS, and Node.'
+//   }, {
+//     name : 'Smart Build System',
+//     info : 'Build system ignores `spec` files, allowing you to keep tests alongside code. Automatic injection of scripts and styles into your index.html'
+//   },  {
+//     name : 'Modular Structure',
+//     info : 'Best practice client and server structures allow for more code reusability and maximum scalability'
+//   },  {
+//     name : 'Optimized Build',
+//     info : 'Build process packs up your templates as a single JavaScript payload, minifies your scripts/css/images, and rewrites asset names for caching.'
+//   },{
+//     name : 'Deployment Ready',
+//     info : 'Easily deploy your app to Heroku or Openshift with the heroku and openshift subgenerators'
+//   });
+// });
 
-Question.find({}).remove(function(){
-  Question.create({
-    questionType:'Sort',
-    questionOption: {optionA: 'This is optionA', optionB: 'This is optionB'}
-  },{
-    questionType: 'Order',
-    questionText: 'This is the text for an order question'
-  },{
-    questionType: 'Match',
-    questionText: 'This is the text for a match question'
-  });
-});
+// Question.find({}).remove(function(){
+//   Question.create({
+//     questionType:'Sort',
+//     questionOption: {optionA: 'This is optionA', optionB: 'This is optionB'}
+//   },{
+//     questionType: 'Order',
+//     questionText: 'This is the text for an order question'
+//   },{
+//     questionType: 'Match',
+//     questionText: 'This is the text for a match question'
+//   });
+// });
 
-User.find({}).remove(function() {
-  User.create({
-    provider: 'local',
-    name: 'Test User',
-    email: 'test@test.com',
-    password: 'test'
-  },{
-    provider: 'local',
-    role: 'admin',
-    name: 'Admin',
-    email: 'admin@admin.com',
-    password: 'admin'
-  }, function() {
-      console.log('finished populating users');
-    }
-  );
-});
+// User.find({}).remove(function() {
+//   User.create({
+//     provider: 'local',
+//     name: 'Test User',
+//     email: 'test@test.com',
+//     password: 'test'
+//   },{
+//     provider: 'local',
+//     role: 'admin',
+//     name: 'Admin',
+//     email: 'admin@admin.com',
+//     password: 'admin'
+//   }, function() {
+//       console.log('finished populating users');
+//     }
+//   );
+// });
 
-Game.find({}).remove(function() {});
+// Game.find({}).remove(function() {});
 
-Group.find({}).remove(function() {
- Group.create({
-   groupName: "tester",
-   users: ["Mike", "Jaimie", "Summer", "Christian", "Andrew", "Gabe", "Omer", "Justin"],
-   questionsArr: [{
-     question: "1",
-     answersArr: [{user: "Mike", answer: "William"}, {user: "Jaimie", answer: "Chloe"}, {user: "Summer", answer: "Michiko"}, {user: "Christian", answer: "Dean"}, {user: "Andrew", answer: "Michael"}, {user: "Gabe", answer: "Laurence"}, {user: "Omer", answer: "Arie"}, {user: "Justin", answer: "Blake"}]
-   }]
- })
-}, function() {
- console.log("it worked!");
-});
+// Group.find({}).remove(function() {
+//  Group.create({
+//    groupName: "tester",
+//    users: ["Mike", "Jaimie", "Summer", "Christian", "Andrew", "Gabe", "Omer", "Justin"],
+//    questionsArr: [{
+//      question: "1",
+//      answersArr: [{user: "Mike", answer: "William"}, {user: "Jaimie", answer: "Chloe"}, {user: "Summer", answer: "Michiko"}, {user: "Christian", answer: "Dean"}, {user: "Andrew", answer: "Michael"}, {user: "Gabe", answer: "Laurence"}, {user: "Omer", answer: "Arie"}, {user: "Justin", answer: "Blake"}]
+//    }]
+//  })
+// }, function() {
+//  console.log("it worked!");
+// });
 
 // Group.find({}).remove(function() {
 //   Group.create({
