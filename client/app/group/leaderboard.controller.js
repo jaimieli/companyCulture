e68de5a6615b398894fb105f02ebed987d3ab62b
Summer McDonald <summermichiko@gmail.com>
2014-10-17 17:55:39 -0400

    styled leaderboard

--- client/app/group/leaderboard.controller.js
+++ client/app/group/leaderboard.controller.js
@@ -1,9 +1,12 @@
 'use strict';
 
 angular.module('companyCultureApp')
-  .controller('LeaderboardController', function($scope, $rootScope, $http){
+  .controller('LeaderboardController', function($scope, $rootScope, $http, async){
     console.log('leaderboard controller')
     var self = this;
+
+    $scope.memberData = [];
+
 
     // currentQuestion data
     $rootScope.$on('data is ready', function(event, data){
@@ -15,19 +18,38 @@ angular.module('companyCultureApp')
       } else {
         self.leaderboardText = 'Last'
       }
-      // get gameTimes for all users who have completed the game
-      // var answersArr = $scope.currentQuestionData.answersArray;
-      // for(var i = 0; i < answersArr.length; i ++) {
-      //   if(answersArr[i].completed === true) {
-      //     console.log('user completed game')
-      //   }
-      // }
     })
     // current Group Data
     $rootScope.$on('groupData ready', function(event, data){
       $scope.groupData = data;
       console.log('$scope.groupData in leaderboard: ', $scope.groupData);
-    })
 
-    // get current times
+      var setMemberData = function(member, done) {
+        var memberObj = {};
+        memberObj.item = member.user;
+        memberObj.bestScore = member.bestTime;
+        $scope.memberData.push(memberObj);
+        done();
+      }
+
+      var complete = function () {
+        console.log("$scope.memberData in complete:", $scope.memberData);
+        console.log("$scope.currentQuestionData in complete:", $scope.currentQuestionData);
+        console.log("finished!");
+        $rootScope.$on("data is ready", function (event, data) {
+          console.log("got into rootScope!!!", data);
+          $scope.currentQuestionData = data;
+          for (var i=0; i<$scope.memberData.length; i++) {
+            for (var j=0; j<$scope.currentQuestionData.answersArray.length; j++) {
+              if($scope.memberData[i].item._id === $scope.currentQuestionData.answersArray[j].user._id) {
+                $scope.memberData[i].currentScore = $scope.currentQuestionData.answersArray[j].gameTime;
+              }
+            }
+          }
+          console.log("$scope.memberData at end:", $scope.memberData);
+        })
+      }
+      async.each($scope.groupData.users, setMemberData, complete);
+      console.log("memberData:", $scope.memberData);
+    })
   })
