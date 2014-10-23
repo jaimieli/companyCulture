2b0f03fce0ca5469aab1c855c1729383d5d91b3a
Jaimie Li <jaimieli@gmail.com>
2014-10-23 18:35:32 -0400

    removed game and thing endpoints and updated question model

--- server/config/socketio.js
+++ server/config/socketio.js
@@ -21,8 +21,6 @@ function onConnect(socket) {
   require('../api/message/message.socket').register(socket);
   require('../api/group/group.socket').register(socket);
   require('../api/question/question.socket').register(socket);
-  require('../api/game/game.socket').register(socket);
-  require('../api/thing/thing.socket').register(socket);
 }
 
 module.exports = function (socketio) {
